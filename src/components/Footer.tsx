02013923c498d03c22de02c89d005c59bdf2538a
peter <20213436+petera2c@users.noreply.github.com>
2024-09-28 19:15:34 -0500

    Footer tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
-const Footer = () => {
+import React from "react";
+
+const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-gray-800 text-white py-6">
       <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2023 My React Table. All rights reserved.</p>
+        <p>&copy; {currentYear} React Simple Table. All rights reserved.</p>
       </div>
     </footer>
   );
