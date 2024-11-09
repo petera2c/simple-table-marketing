680920a2d785606eb9107155b6fd24e9e598ee63
peter <20213436+petera2c@users.noreply.github.com>
2024-11-09 15:24:40 -0500

    Adding assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import { SimpleTable } from "simple-table-core";
 import "simple-table-core/dist/style.css";
-
-export const SAMPLE_HEADERS: any[] = [
-  { label: "Product ID", accessor: "id", width: 150 },
-  { label: "Product Name", accessor: "productName", width: 200 },
-  { label: "Category", accessor: "category", width: 150 },
-  { label: "Quantity", accessor: "quantity", width: 100 },
-];
+// import "./style-overrides.css";
+import SimpleTableExample from "./SimpleTableExample";
 
 function App() {
   return (
-    <SimpleTable
-      defaultHeaders={SAMPLE_HEADERS}
-      height="auto"
-      // height="calc(100dvh - 4rem)"
-      rows={[
-        {
-          id: 1,
-          productName: "Product 1",
-          category: "Category 1",
-          quantity: 10,
-        },
-      ]}
-      // shouldPaginate={false}
-    />
+    <div style={{ padding: "2rem" }}>
+      <SimpleTableExample />
+    </div>
   );
 }
 
