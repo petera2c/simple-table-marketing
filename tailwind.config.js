dc286a4f1ec7e8ab8507fdb4a2be9c16ab7ccc61
peter <20213436+petera2c@users.noreply.github.com>
2025-04-12 22:34:01 -0500

    Switiching to simpler tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./client/src/**/*.{html,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
