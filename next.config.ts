e82f158eea104c840d5af75789397249bb29e000
peter <20213436+petera2c@users.noreply.github.com>
2025-06-29 01:10:38 -0500

    Build fixes

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,17 @@ const config: NextConfig = {
     };
     return config;
   },
+  transpilePackages: [
+    "antd",
+    "@ant-design",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-notification",
+    "rc-tooltip",
+    "rc-tree",
+    "rc-table",
+  ],
 };
 
 export default config;
